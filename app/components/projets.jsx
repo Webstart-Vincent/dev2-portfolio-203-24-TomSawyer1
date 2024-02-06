daf42e453f22afb9645cbe72ee9080ac0655a034
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2024-02-06 20:14:06 +0100

    poo

---
+++ app/components/projets.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Projets() {
+  return (
+    <div className='grid grid-rows-2 gap-4'>
+      <div className=''></div>
+
+      <div className='flex flex-nowrap'>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Projets
