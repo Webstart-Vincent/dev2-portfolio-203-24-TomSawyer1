df2aaa6d6d8d889ee2c1a3ef05eba90e9f7fa9e9
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2024-07-02 15:18:30 +0200

    text

--- components/detail.jsx
+++ components/detail.jsx
@@ -20,12 +20,12 @@ function Detail({ project }) {
       <div className="flex flex-col place-content-center items-center">
         <h1 className="font-sans text-white mb-4 text-lg sm:text-2xl lg:text-3xl">{project.title}</h1>
         <div>
-          {project.text}
+          <p className='text-white'>{project.text}</p>
         </div>
         <div>
           <Link href={project.site} passHref>
             <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 via-blue-800 to-red-500 hover:bg-slate-800 text-white mt-3'>
-              <span className='block bg-white hover:bg-slate-800 rounded-full py-3 px-2'>Aller sur le site</span>
+              <span className='block bg-black hover:bg-slate-800 rounded-full py-3 px-2'>Aller sur le site</span>
             </button>
           </Link>
         </div>
